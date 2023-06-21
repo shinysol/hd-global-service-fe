f58adf5e2d0256f468d87b1191d3e4f2d2a3c19a
Renny H. Park <soleiltude@gmail.com>
2023-06-22 00:54:52 +0900

    go public

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,8 +1,8 @@
 <template>
   <footer
-    class="w-full p-2 h-20 bg-gray-50 flex justify-between items-center text-sm text-gray-400"
+    class="w-full p-2 h-20 flex justify-between items-center text-sm bg-blue-100"
   >
-    48058) 79 Centum jungang-ro, U-dong, Haeundae-gu, Busan
-    <div class="flex items-center">Terms Of Use | Privacy Policy</div>
+    주소
+    <div class="flex items-center">sides</div>
   </footer>
 </template>
--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="w-full p-2 flex justify-between items-center h-20 bg-blue-700">
     <div class="text-white px-4">
-      <a href="/">HD Cloud</a>
+      <a href="/">사이트</a>
     </div>
     <Nav />
   </header>
